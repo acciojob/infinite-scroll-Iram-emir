dfb73e035b64fd7f5cd8a9f94c8429e391c775c9
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-29 00:37:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+let doc = document.getElementById('infi-list');
+let i;
+
+
+for (i = 1; i <= 10; i++) {
+    doc.innerHTML += `<li>Item ${i}</li>`;
+}
+
+let a = doc.lastChild;
+
+a.addEventListener('mouseover', () => {
+	
+        for (let j = 11; j <= 12; j++) {
+            doc.innerHTML += `<li>Item ${j}</li>`;
+        }
+   
+});
 
